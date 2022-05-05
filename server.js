bef47629c418d3b96c30ad9d7b64a159bb80f323
Dean Thompson <deanthompson.music@gmail.com>
2022-05-05 20:48:57 +0900

    Still having connection error

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var app = express()
 var Data = require("./noteSchema")
 
 //connect mongoose to mongoDB
-mongoose.connect("mongodb://localhost:27017/newDB")
+mongoose.connect("mongodb://localhost:27017/noteDB", {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
 
 mongoose.connection.once("open", () => {
     console.log("Connected to database!")
@@ -16,7 +18,7 @@ console.log("Failed to connect " + error)
 
 //http://192.168.0.10:8081/create
 
-var server = app.listen(8081, "10.167.91.39", () => {
+var server = app.listen(8081, "192.168.0.10", () => {
     console.log("Server is running!")
 })
 
